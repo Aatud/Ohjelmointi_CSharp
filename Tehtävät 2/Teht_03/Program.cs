8127a20a265608059b58a798e6d2c9dfa574e3ef
Aatud <aatu.m@netti.fi>
2017-01-21 20:26:06 +0200

    3 Tehtävän postaus

---
+++ Tehtävät 2/Teht_03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Luku;
+            int i, k, a, w;
+            int e = 0;
+            Console.Write("Anna rivien määrä > ");
+            Luku = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= Luku; i++)
+            {
+                for (a = 1; a < Luku-i+1; a++)
+                {
+                    Console.Write(" ");
+                }            
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                for (k = 2; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            while (e < 2)
+            {
+                for (w = 1; w < Luku; w++)
+                {
+                    Console.Write(" ");
+                    
+                }
+                Console.WriteLine("*");
+                e++;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
