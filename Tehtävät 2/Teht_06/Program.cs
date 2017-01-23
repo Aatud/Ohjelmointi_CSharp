001e5d560a9543c53a2c7b32d4932b41a80f445b
Aatu Muukka <aatu.m@netti.fi>
2017-01-23 15:31:25 +0200

    Tehtävä 6 postaus (kesken)

---
+++ Tehtävät 2/Teht_06/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sana;
+            
+            Console.Write("Anna sana jonka haluat testata > ");
+            sana = Console.ReadLine();
+            string anas = "";
+            int q = sana.Length - 1;
+            for (int i = 0; i < sana.Length; i++)
+            {
+                //anas = sana[q].Join.ToString();
+                anas = String.Join("", sana[q]);
+                Console.Write(sana[q]);
+                q--;
+            }
+            if (sana == anas)
+            {
+                Console.WriteLine("Sana on palidromi");
+            }
+            else {Console.WriteLine("Sana ei ole palidromi");}
+            Console.Write(anas+"\n" + sana);
+            Console.ReadLine();
+            
+        }
+    }
+}
