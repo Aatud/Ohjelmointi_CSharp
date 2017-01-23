d64a524eda5a6891937d3ecb0fd0c7c41dc21148
Aatud <aatu.m@netti.fi>
2017-01-23 13:00:58 +0200

    Tehtävä 4 postaus

---
+++ Tehtävät 2/Teht_04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arvm = 0;
+            int arvaus = 101;
+            Random rnd = new Random();
+            int vastaus = rnd.Next(0, 100);
+            Console.WriteLine("Arvaa luku väliltä 0-100");
+            
+
+            while (arvaus != vastaus)
+            {
+                Console.Write("Anna arvaus > ");
+                arvaus = int.Parse(Console.ReadLine());
+                if (arvaus < vastaus) Console.WriteLine("Luku on suurempi.");
+                if (arvaus > vastaus) Console.WriteLine("Luku on pienempi.");
+                arvm++;
+            }
+            Console.WriteLine("Hienoa, arvasit luvun oikein {0}. kerralla.", arvm);
+
+
+            Console.WriteLine(vastaus);
+            Console.ReadLine();
+        }
+    }
+}
