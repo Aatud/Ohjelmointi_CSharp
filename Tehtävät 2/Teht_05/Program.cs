6b88846f28edfb665322d1a6885b0c1f20681044
Aatud <aatu.m@netti.fi>
2017-01-23 13:50:15 +0200

    Tehtävä 5 postaus

---
+++ Tehtävät 2/Teht_05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taulu1 = new int[] { 10, 20, 30, 40, 50 };
+            int[] taulu2 = new int[] { 5, 15, 25, 35, 45 };
+            int[] taulu_y = new int[taulu1.Length + taulu2.Length];
+
+            Array.Copy(taulu1, taulu_y, taulu1.Length);
+            Array.Copy(taulu2, 0, taulu_y, taulu1.Length, taulu2.Length);
+            Array.Sort(taulu_y);
+            Console.Write("Luvut yhdistetyssä taulukossa : ");
+            for ( int i = 0; i < taulu_y.Length; i++)
+            {
+                Console.Write(taulu_y[i]);
+                if (i != taulu_y.Length - 1) Console.Write(",");
+            }
+            Console.ReadLine();
+        }
+    }
+}
