2098c73c5dac7033cfd5b8d1df53277dbc82c349
Aatud <aatu.m@netti.fi>
2017-01-26 01:42:28 +0200

    Teht 1

---
+++ Tehtävät 3/Teht_01/Kiuas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_01
+{
+    class Kiuas
+    {
+        //Ominaisuudet
+        public string Malli { get; set; }
+        public string Väri { get; set; }
+        public bool OnOff { get; set; }
+        public int Lämpötila { get; set; }
+        public int Kosteus { get; set; }
+
+        //Toiminnot
+
+        public void On()
+        {
+            OnOff = true;
+        }
+        public void Off()
+        {
+            OnOff = false;
+        }
+        public void Tulosta()
+        {
+            Console.WriteLine("Kiuas asetukset:");
+            Console.WriteLine("- Malli : " + Malli);
+            Console.WriteLine("- Väri : " + Väri);
+            Console.WriteLine("- Päällä : " + OnOff);
+            Console.WriteLine("- Lämpötila : " + Lämpötila);
+            Console.WriteLine("- Kosteus : " + Kosteus);
+        }
+    }
+}
---
+++ Tehtävät 3/Teht_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas hotstone = new Kiuas();
+            hotstone.Malli = "Hot stone 2000";
+            hotstone.Väri = "Punainen";
+            hotstone.OnOff = true;
+            hotstone.Lämpötila = 90;
+            hotstone.Kosteus = 5;
+
+            hotstone.Tulosta();
+            Console.ReadLine();
+
+            hotstone.Off();
+            hotstone.Tulosta();
+            Console.ReadLine();
+        }
+    }
+}
