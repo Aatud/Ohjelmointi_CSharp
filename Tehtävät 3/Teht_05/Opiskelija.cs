eaeae1eac644dd6a5e1564db755a7740baabd52b
Aatud <aatu.m@netti.fi>
2017-01-30 13:37:42 +0200

    Teht_05 bump (unfinished)

---
+++ Tehtävät 3/Teht_05/Opiskelija.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_05
+{
+    class Opiskelija
+    {
+        public string Nimi { get; set; }
+        public int Ikä { get; set; }
+        public string Sukupuoli { get; set; }
+        public string OpTunnus { get; set; }
+        public string Tila { get; set; }
+
+        public void Kasvaa()
+        {
+            Ikä += 1;
+        }
+        public void Nukkuu()
+        {
+            Tila = "Nukkumassa";
+        }
+        public void Kaljalla()
+        {
+            Tila = "Kaljalla";
+        }
+        public void Syö()
+        {
+            Tila = "Syömässä";
+        }
+
+    }
+}
---
+++ Tehtävät 3/Teht_05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Opiskelijat = new char[500];
+            Opiskelija Masa = new Opiskelija();
+            Masa.Nimi = "Masa";
+            Masa.Ikä = 20;
+            Masa.Sukupuoli = "Mies";
+            Masa.OpTunnus = "B123";
+            Masa.Kaljalla();
+            string MasaT = Masa.Nimi;
+            Opiskelijat = MasaT.ToCharArray();
+            Console.Write(Opiskelijat[0]);
+            Console.ReadLine();
+        }
+    }
+}
