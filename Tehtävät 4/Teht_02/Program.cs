9162409a157876e72f7aa4ea76c14dfccd9c4242
Aatud <aatu.m@netti.fi>
2017-02-04 16:42:44 +0200

    Teht_02 bump

---
+++ Tehtävät 4/Teht_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vahvistin vahvstin = new Vahvistin();
+            bool result;
+
+            do
+            {
+                
+                Console.Write("Give a new volume value (0-100) > ");
+                
+                string line = Console.ReadLine();
+                int volume;
+                result = int.TryParse(line, out volume);
+                if (result)
+                {
+                    if (volume < 100 && volume > 0)
+                    {
+                        vahvstin.Volume = volume;
+                        Console.WriteLine("Amplifier volume is set to : " + vahvstin.Volume);
+                    }
+                    vahvstin.Volume = volume;                                        
+                }                
+            }
+            while (result);
+        }
+    }
+}
---
+++ Tehtävät 4/Teht_02/Vahvistin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_02
+{
+    class Vahvistin
+    {
+        private const int minVolume = 0;
+        private const int maxVolume = 100;
+        private int volume = 50;
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value < minVolume)
+                {
+                    Console.WriteLine("Too low volume - Amplifier volume is set to minimum : "+ minVolume);
+                }
+                else if (value > maxVolume)
+                {
+                    Console.WriteLine("Too much volume - Amplifier volume is set to maximum : "+ maxVolume);
+                }
+                else if (value < maxVolume || value > minVolume)
+                {
+                    volume = value;
+                }
+            }
+        }
+    }
+}
