05b3b1b80b0ee2988a71371129b8c16e6c0f7524
Aatu Muukka <aatu.m@netti.fi>
2017-02-13 16:56:57 +0200

    Something went wrong

---
+++ Tehtävät 5/Teht_01/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_01
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int RegisterNum { get; set; }
+        public string Address { get; set; }
+        public string HairColor { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstname, string lastname, int age, int registernum, string address, string haircolor)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Age = age;
+            RegisterNum = registernum;
+            Address = address;
+            HairColor = haircolor;
+        }
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Age + " " + RegisterNum + " " + Address + " " + HairColor;
+        }
+    }
+}
---
+++ Tehtävät 5/Teht_01/Persons.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_01
+{
+    class Persons
+    {
+
+        public List<Persons> persons;
+
+        public Persons()
+        {
+
+        }
+
+        public void AddPerson()
+        {
+            persons.Add(new Person{FirstName, LastName, Age, RegisterNum, Address, HairColor});
+        }
+        public void GetPerson()
+        {
+
+        }
+        public void FindPerson()
+        {
+
+        }
+        public void PrintData()
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Age;
+        }        
+    }
+}
