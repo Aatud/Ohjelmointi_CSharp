c18b3a38ccd697c2c3024d536f9e798960996728
H9550 <H9550@ICT-421-WS-10>
2017-03-06 17:45:26 +0200

    bumb

---
+++ Tehtävät 6/teht_02/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht_02
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+
+        public Car(string name)
+        {
+            Name = name;
+            Count = 0;
+        }
+
+        public void Add()
+        {
+            Count++;
+        }
+        public void Reset()
+        {
+            Count = 0;
+        }
+    }
+}
---
+++ Tehtävät 6/teht_02/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace teht_02
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Car truck = new Car("Sisu");
+        Car car = new Car("Lada");
+        public MainPage()
+        {
+            this.InitializeComponent();
+            truckButton.Content = truck.Name;
+            carButton.Content = car.Name;
+        }
+        private void truckButton_Click(object sender, RoutedEventArgs e)
+        {
+            truck.Add();
+            truckTextBlock.Text = truck.Count.ToString();
+        }
+
+        private void carButton_Click(object sender, RoutedEventArgs e)
+        {
+            car.Add();
+            carTextBlock.Text = car.Count.ToString();
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            truck.Reset();
+            car.Reset();
+            truckTextBlock.Text = truck.Count.ToString();
+            carTextBlock.Text = car.Count.ToString();
+        }
+    }
+}
