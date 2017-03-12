d89fa9e79907ee6e21b879935c192ab7c6869639
aatud <H9550@student.jamk.fi>
2017-03-13 00:12:12 +0200

    Teht_03 bump

---
+++ Tehtävät 8/Teht_03/Lotto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_03
+{
+    class Lotto
+    {
+        Random rand;
+        //public List<int> numerot { get; set; }
+        public Lotto()
+        {
+            rand = new Random();
+        }
+        public List<int> Gen()
+        {
+            int arvottunumero = 0;
+            List<int> numerot = new List<int>();
+            for (int i=0; i < 7; i++)
+            {
+                arvottunumero = rand.Next(1, 39);
+                if (numerot.Contains(arvottunumero))
+                {
+                    i--;
+                }
+                else numerot.Add(arvottunumero);                
+            }
+            return numerot;
+        }
+    }
+}
---
+++ Tehtävät 8/Teht_03/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teht_03
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, RoutedEventArgs e)
+        {
+            Lotto uusi = new Lotto();
+            for (int i = 0; i < int.Parse(numDrawtextBox.Text); i++)
+            {
+                List<int> rivi = uusi.Gen();
+                lottorivitextBox.Text += "Rivi "+(i+1).ToString()+": ";
+                foreach (int n in rivi)
+                {
+                    lottorivitextBox.Text += n.ToString() + " ";
+                }
+                lottorivitextBox.Text += Environment.NewLine;
+                Scroll.Content = (lottorivitextBox);
+            }
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            numDrawtextBox.Text = "";
+            lottorivitextBox.Text = "";
+        }
+    }
+}
