3791634012cd4042145c5f3dfe2f8b40904c00c9
aatud <H9550@student.jamk.fi>
2017-03-13 03:52:48 +0200

    Teht_04

---
+++ Tehtävät 8/Teht_04/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teht_04
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            button0.Click += button;
+            button1.Click += button;
+            button2.Click += button;
+            button3.Click += button;
+            button4.Click += button;
+            button5.Click += button;
+            button6.Click += button;
+            button7.Click += button;
+            button8.Click += button;
+            button9.Click += button;
+        }
+        
+
+        bool i = false;
+                    
+        private void button(object sender, RoutedEventArgs e)
+        {
+            if (i == false)
+            {
+                lukutextBox.Text = "";
+            }
+            string buttonString = (((Button)sender).Content).ToString();
+            lukutextBox.Text += buttonString;
+            i = true;
+        }
+
+        private void buttonremove_Click(object sender, RoutedEventArgs e)
+        {
+            string line = lukutextBox.Text;
+            if (line.Length > 1)
+            {
+                line = line.Substring(0, line.Length - 1);
+                lukutextBox.Text = line;
+            }
+            else
+            {
+                lukutextBox.Text = "0";
+                i = false;
+            }
+        }
+
+        private void buttonok_Click(object sender, RoutedEventArgs e)
+        {            
+            kiuas setting = new kiuas();
+            if ((bool)radioButtonTemp.IsChecked)
+            {
+                try
+                {
+                    setting.Temperature = double.Parse(lukutextBox.Text);
+                    textBlock2.Text = setting.Temperature.ToString();
+                    if (setting.Temperature == 0)
+                    {
+                        textBlock4.Text = "Info: Syöttämäsi arvo on liian suuri.";
+                    }
+                }
+                catch
+                {
+                    textBlock4.Text = "Info: Arvo ei ole numero!";
+                }
+                finally
+                {                    
+                }                            
+            }
+
+            if ((bool)radioButtonHum.IsChecked)
+            {
+                try
+                {
+                    setting.Humidity = double.Parse(lukutextBox.Text);
+                    textBlock3.Text = setting.Humidity.ToString();
+                    if (setting.Humidity == 0) textBlock4.Text = "Info: Syöttämäsi arvo on liian suuri.";
+                }
+                catch
+                {
+                    textBlock4.Text = "Info: Arvo ei ole numero!";
+                }
+                finally
+                {
+                }                
+            }
+            lukutextBox.Text = "0";
+            i = false;
+
+        }
+
+        private void buttondot_Click(object sender, RoutedEventArgs e)
+        {
+            lukutextBox.Text += ".";
+            i = true;
+        }
+    }
+}
---
+++ Tehtävät 8/Teht_04/kiuas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_04
+{
+    class kiuas
+    {
+        private readonly double MaxTemp = 120.00;
+        private readonly double MinTemp = 0.00;
+        private readonly double MaxHum = 100.0;
+        private readonly double MinHum = 0.0;
+        private double temperature;
+        private double humidity;
+        public double Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                
+                if (value <= MaxTemp && value >= MinTemp)
+                {
+                    temperature = value;
+                }
+                else
+                {
+                    temperature = MinTemp;
+                }
+            }
+            
+        }
+        public double Humidity
+        {
+            get
+            {
+                return humidity;
+            }
+            set
+            {
+                if (value <= MaxHum && value >= MinHum) humidity = value;
+                else
+                {
+                    humidity = MinHum;
+                }
+            }
+        }
+    }
+}
