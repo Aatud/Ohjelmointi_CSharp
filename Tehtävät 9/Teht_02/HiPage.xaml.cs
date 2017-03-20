ba8d2ff939efe927c0145a9cefa4b2015f9304ac
aatud <H9550@student.jamk.fi>
2017-03-20 17:07:31 +0200

    Part 9

---
+++ Tehtävät 9/Teht_02/HiPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Teht_02
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HiPage : Page
+    {
+        public HiPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is Person)
+            {
+                Person person = (Person)e.Parameter;
+                textBlockGreeting.Text = "Hi, " + person.Etunimi + " " + person.Sukunimi;
+            }
+            else
+            {
+                textBlockGreeting.Text = "Hi!";
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            (App.Current as App).Number = rand.Next();
+
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) return;
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+    }
+}
---
+++ Tehtävät 9/Teht_02/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_02
+{
+    class Person
+    {
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
